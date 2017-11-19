986de11664f72667b5f7ecfc0422b7809f0ddcf7
medobelk <cynerdemid@gmail.com>
2017-11-19 19:23:32 +0200

    places for uk now

--- app/Http/Controllers/ObjectsController.php
+++ app/Http/Controllers/ObjectsController.php
@@ -262,12 +262,13 @@ class ObjectsController extends Controller
                     if (strlen($objectData['latitude']) < 2 || strlen($objectData['longitude']) < 2) {
 
                         $addressForRequest = str_replace(' ', '+', $importData[$arrIndex]['address']);
-                        $json = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $addressForRequest . '&key=AIzaSyDQn2uArxofxG0oJWMNDgTxbMc17zWAXnA');
+                        $json = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $addressForRequest . '&key=AIzaSyDQn2uArxofxG0oJWMNDgTxbMc17zWAXnA&language=uk');
                         $objectDataAPI = json_decode($json);
                         //mykey               AIzaSyAECoGPJKuBmmc4_Y0PjKkWRLUjheLqwAI
                         //rightkey            AIzaSyCylzj30nQuaMwhN6Xeqf7wrSYV7KR0yFs
                         //palces libraty key  AIzaSyAzxY-YzRtwoQs6_Q9hHk2ZSo2sWtIIFGM
-                        //the most right key from google after card verify
+                        //the most right key  AIzaSyDQn2uArxofxG0oJWMNDgTxbMc17zWAXnA
+                        // from google after card verify 
                     }
                 } else {
                     $objectDataAPI = 'COORDINATES_EXIST';

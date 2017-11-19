14b41791d53328008d46097ef5480bd83a1e1456
medobelk <cynerdemid@gmail.com>
2017-11-19 19:47:04 +0200

    game of key

--- app/Http/Controllers/ObjectsController.php
+++ app/Http/Controllers/ObjectsController.php
@@ -262,7 +262,7 @@ class ObjectsController extends Controller
                     if (strlen($objectData['latitude']) < 2 || strlen($objectData['longitude']) < 2) {
 
                         $addressForRequest = str_replace(' ', '+', $importData[$arrIndex]['address']);
-                        $json = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' . $addressForRequest . '&key=AIzaSyDQn2uArxofxG0oJWMNDgTxbMc17zWAXnA&language=uk');
+                        $json = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDQn2uArxofxG0oJWMNDgTxbMc17zWAXnA&query=' . $addressForRequest . '&language=uk');
                         $objectDataAPI = json_decode($json);
                         //mykey               AIzaSyAECoGPJKuBmmc4_Y0PjKkWRLUjheLqwAI
                         //rightkey            AIzaSyCylzj30nQuaMwhN6Xeqf7wrSYV7KR0yFs

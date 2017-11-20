98c96cad1cc9ae198c2d6676f74d560625802393
medobelk <cynerdemid@gmail.com>
2017-11-20 13:33:53 +0200

    changed url of facebook app to newer

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -9,7 +9,7 @@
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = "//connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v2.10&appId=1509381815774111";
+        js.src = "//connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v2.11&appId=1509381815774111";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
